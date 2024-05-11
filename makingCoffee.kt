d26fd77838702ab7c5f12cb4b056dcbbb7de0908
Nikithathota <121127838+Nikithathota@users.noreply.github.com>
2024-05-11 17:41:45 +0530

    Update makingCoffee.kt

--- makingCoffee.kt
+++ makingCoffee.kt
@@ -1,5 +1,79 @@
 package com.example.kotinbasics
 
+data class CoffeeDetails(
+    val sugarCount:Int,
+    val name:String,
+    val size:String,
+    val creamAmount:Int
+    )
+
+
+
+fun main(){
+    val coffeeForDenis=CoffeeDetails(sugarCount = 0, name = "Denis", size = "xxl", creamAmount = 0)
+    makeCoffee(coffeeForDenis)
+
+
+}
+
+
+fun askCoffeeDetails(){
+    println("who is this coffee for?")
+    val name= readln()
+    println("How many pieces of sugar do you want?")
+    val sugarCount= readln()
+    val sugarCountInt=sugarCount.toInt()
+    //makeCoffee(sugarCountInt,name)
+}
+
+
+
+fun makeCoffee(coffeeDetails: CoffeeDetails){
+    if(coffeeDetails.sugarCount==1){
+        println("Coffee with ${coffeeDetails.sugarCount} "+
+                "spoon of sugar for ${coffeeDetails.name} + and cream:"+
+                "${coffeeDetails.creamAmount}")
+    }
+    else if(coffeeDetails.sugarCount==0){
+        println("coffee with no sugar for ${coffeeDetails.name} + and cream:" +
+                "${coffeeDetails.creamAmount}")
+    }
+    else {
+        println("Coffee with ${coffeeDetails.sugarCount} spoons of sugar for ${coffeeDetails.name} + and cream:" +
+                "${coffeeDetails.creamAmount}")
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 fun main(){
     println("who is this coffee for?")
     val name= readln()
@@ -19,6 +93,7 @@ fun makeCoffee(sugarCount:Int, name:String){
         println("Coffee with $sugarCount spoons of sugar for $name")
     }
 }
+*/
 
 
 
